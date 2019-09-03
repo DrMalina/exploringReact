1a89fc76720fdec13dcdf6a5ba128df19ab9ccce
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-09-03 21:08:40 +0200

    pics

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} action="" className="ui form">
+          <div className="field">
+            <label htmlFor="text">Image Search</label>
+            <input
+              type="text"
+              name="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
