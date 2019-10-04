908e91d3b7ef1917835281084ba041f94d894899
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-10-04 23:00:00 +0200

    Remove project, not correct repo

--- recipes/src/api/edamam.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const KEY = "dd8516e2403e51b4b41168577f16526a";
-export const ID = "9e7a6b5a";
-
-export default axios.create({
-  baseURL: "https://api.edamam.com"
-});
